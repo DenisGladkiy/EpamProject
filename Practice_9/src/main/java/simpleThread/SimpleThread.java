de65a815a210a99ead45e0efe3fed6f88f1dfb25
DenisGladkiy <gladkiy@gmail.com>
2018-04-17 23:41:45 +0300

    Practice 9.1

---
+++ Practice_9/src/main/java/simpleThread/SimpleThread.java
@@ -0,0 +1,22 @@
+package simpleThread;
+
+/**
+ * Created by Denis on 17.04.2018.
+ */
+public class SimpleThread {
+
+    public static void main(String[] args) {
+        Runnable simpleRunnable = () -> {
+            for(int i = 10; i >=0 ; i--) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Boooom!");
+        };
+        new Thread(simpleRunnable).start();
+    }
+}
